31371d69d26fabfe5c82fa67e0d1be80b034360b
Zone-OTP <zezva.mebagishvili5@mail.ru>
2025-11-15 15:55:48 +0400

    steps 1. thru 6.3 done

---
+++ src/main/java/ge/tbc/testautomation/HelloWorld.java
@@ -0,0 +1,8 @@
+package ge.tbc.testautomation;
+//package com.example; "not possible to add, different package"
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+}
